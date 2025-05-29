0c6c1b4778fd7ba8099e67dd541c91ea5d5daf2a
anmaiden <aannna123@gmail.com>
2025-05-29 22:18:11 +0300

    fixes brauserRouter

--- src/AppRoutes.tsx
+++ src/AppRoutes.tsx
@@ -4,24 +4,21 @@ import CurrentMonthCalendarView from './modules/CurrentMonthCalendarModule/compo
 import WorkoutYouTubeModule from './modules/WorkoutYouTubeModule/components/WorkoutYouTubeModule';
 import ActivityHistory from './modules/ActivityHistoryModule/components/ActivityHistory';
 import ProfileDashboard from './components/ProfileDashboard/ProfileDashboard';
-import { BrowserRouter } from 'react-router-dom';
 
 const AppRoutes = () => {
   return (
-    <BrowserRouter basename="/move-with-calendar">
-      <Routes>
-        <Route path="/" element={<ProfileDashboard />} />
-        <Route
-          path="/current-month-calendar"
-          element={<CurrentMonthCalendarView />}
-        />
-        <Route
-          path="/playlists"
-          element={<WorkoutYouTubeModule key="youtube-module" />}
-        />
-        <Route path="/activity_history" element={<ActivityHistory />} />
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/" element={<ProfileDashboard />} />
+      <Route
+        path="/current-month-calendar"
+        element={<CurrentMonthCalendarView />}
+      />
+      <Route
+        path="/playlists"
+        element={<WorkoutYouTubeModule key="youtube-module" />}
+      />
+      <Route path="/activity_history" element={<ActivityHistory />} />
+    </Routes>
   );
 };
 
